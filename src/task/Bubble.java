2a5c444163ac525acf594d5d9a10f4b862934515
TetianaReznikova <reznikovat2705@gmail.com>
2023-09-12 15:20:55 +0300

    Initial commit

---
+++ src/task/Bubble.java
@@ -0,0 +1,43 @@
+package task;
+
+import java.util.Scanner;
+
+public class Bubble {
+    static int dayCounter;
+    static double[] price;
+    private final static String CURRENCY = "UAH";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // n - кількість днів
+        int n = 3;
+        double sum = 0;
+        price = new double[n];
+        System.out.println("Enter the price: ");
+        for (int i = 0; i < n; ++i) {
+            dayCounter++;
+            System.out.println("Day " + dayCounter + "-");
+            price[i] = sc.nextDouble();
+            sum += price[i];
+        }
+sc.close();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j < n - i; ++j) {
+                if (price[j - 1] > price[j]) {
+                    double temp = price[j - 1];
+                    price[j - 1] = price[j];
+                    price[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted price:");
+        for (int i = 0; i < n; ++i) {
+            System.out.printf("%.2f %s%n",
+                    price[i], CURRENCY);
+        }
+
+        double averr = sum / dayCounter;
+        System.out.printf("Averrage price equals: %.2f %s", averr, CURRENCY);
+    }
+}
